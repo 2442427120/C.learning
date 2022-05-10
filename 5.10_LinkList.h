6c52165ec6d56bd28a79ad14ca6a562407389bd4
2442427120 <102736244+2442427120@users.noreply.github.com>
2022-05-10 23:14:16 +0800

    线性表的链式存储——单链表
    实现单链表的增删改查

---
+++ 5.10_LinkList.h
@@ -0,0 +1,40 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+typedef void status;
+typedef int ElemType;
+
+typedef struct Node
+{
+	ElemType data;
+	struct Node* next;
+}LNode;
+
+typedef struct Node* LinkList;
+
+//初始化线性表
+status Init_LinkList(LinkList* L);
+//头插法创建线性表
+status Creat_LinkList_Head(LinkList* L);
+//尾插法创建线性表
+status Creat_LinkList_Tail(LinkList* L);
+//打印线性表
+status Print_LinkList(LinkList* L);
+//查找线性表中的某元素
+status Serach_by_Elem(LinkList* L);
+//修改某一位置元素的值
+//status Modify_by_seat(LinkList* L);
+//按位查找元素
+status Search_by_Pos(LinkList* L);
+//插入元素到线性表
+status Inser_Elem(LinkList *L);
+//删除线性表中的元素
+status Delete_Elem(LinkList* L);
---
+++ 5.10_test.c
@@ -0,0 +1,67 @@
+#include "5.10_LinkList.h"
+
+status menu(void)
+{
+	printf("**************************************************\n");
+	printf("*******        线性表的链式存储            *******\n");
+	printf("*******        1.头插法创建线性表          *******\n");
+	printf("*******        2.尾插法创建线性表          *******\n");
+	printf("*******        3.打印线性表                *******\n");
+	printf("*******        4.按值查询线性表中的元素    *******\n");
+	printf("*******        5.按位查询线性表中的元素    *******\n");
+	printf("*******        6.插入元素                  *******\n");
+	printf("*******        7.删除元素                  *******\n");
+	printf("*******        0.退出                      *******\n");
+	printf("**************************************************\n");
+}
+
+int main(void)
+{
+	LinkList L;
+	L = (LinkList)malloc(sizeof(LNode));
+	if (L == NULL)
+	{
+		return;
+	}
+	int choose = 0;
+	Init_LinkList(L);
+	do
+	{
+		menu();
+		printf("请选择相应的功能:>");
+		scanf("%d", &choose);
+
+		switch (choose)
+		{
+		case 1:
+			Creat_LinkList_Head(L);
+			break;
+		case 2:
+			Creat_LinkList_Tail(L);
+			break;
+		case 3:
+			Print_LinkList(L);
+			break;
+		case 4:
+			Serach_by_Elem(L);
+			break;
+		case 5:
+			Search_by_Pos(L);
+			break;
+		case 6:
+			Inser_Elem(L);
+			break;
+		case 7:
+			Delete_Elem(L);
+			break;
+		case 0:
+			printf("退出成功\n");
+			break;
+		default:
+			printf("输入的值非法，请重新输入\n");
+			break;
+		}
+	} while (choose);
+
+	return 0;
+}
