1529a4396feb7e88298ab246b885fb44241408ad
2442427120 <102736244+2442427120@users.noreply.github.com>
2022-09-28 23:33:45 +0800

    Add files via upload

---
+++ C++学习代码/9.28/use_stui.cpp
@@ -0,0 +1,39 @@
+////私有继承
+////在studentc的基础上进行修改
+//#include "studenti.h"
+//
+//const int pupils = 3;
+//const int quizzes = 5;
+//
+//void set(Student& sa, int n);
+//
+//int main(void)
+//{
+//	Student ada[pupils] = { Student(quizzes), Student(quizzes), Student(quizzes) };
+//
+//	int i;
+//	for (i = 0; i < pupils; i++)
+//		set(ada[i], quizzes);
+//	cout << "\nStudent List:\n";
+//	for (i = 0; i < pupils; i++)
+//		cout << ada[i].Name() << endl;
+//	cout << "\nResult:";
+//	for (i = 0; i < pupils; i++)
+//	{
+//		cout << endl << ada[i];
+//		cout << "average: " << ada[i].Average() << endl;
+//	}
+//	cout << "Done\n";
+//
+//	return 0;
+//}
+//
+//void set(Student& sa, int n)
+//{
+//	cout << "Please enter the student's name: ";
+//	getline(cin, sa);
+//	cout << "Please enter " << n << " quiz scores:\n";
+//	for (int i = 0; i < n; i++)
+//		cin >> sa[i];
+//	while (cin.get() != '\n');
+//}
