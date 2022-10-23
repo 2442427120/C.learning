b686477f39723547e996c5d786a84153d109c5cf
2442427120 <102736244+2442427120@users.noreply.github.com>
2022-10-23 19:15:33 +0800

    Add files via upload

---
+++ C++学习代码/10.23/sales.cpp
@@ -0,0 +1,66 @@
+#include "sales.h"
+
+Sales::Sales(int yy)
+{
+	year = yy;
+	for (int i = 0; i < MONTHS; i++)
+		gross[i] = 0;
+}
+
+Sales::Sales(int yy, const double* gr, int n)
+{
+	year = yy;
+	int lim = (n < MONTHS) ? n : MONTHS;
+	int i;
+	for (i = 0; i < lim; i++)
+		gross[i] = gr[i];
+	for (; i < MONTHS; i++)
+		gross[i] = 0;
+}
+
+double Sales::operator[](int i) const
+{
+	if (i < 0 || i >= MONTHS)
+		throw bad_index(i);
+	return gross[i];
+}
+
+double& Sales::operator[](int i)
+{
+	if (i < 0 || i >= MONTHS)
+		throw bad_index(i);
+	return gross[i];
+}
+
+Sales::bad_index::bad_index(int ix, const string& s) : logic_error(s), bi(ix)
+{
+}
+
+LabeledSales::LabeledSales(const string& lb, int yy) : Sales(yy)
+{
+	label = lb;
+}
+
+LabeledSales::LabeledSales(const string& lb, int yy, const double* gr, int n) : Sales(yy, gr, n)
+{
+	label = lb;
+}
+
+double LabeledSales::operator[](int i) const
+{
+	if (i < 0 || i >= MONTHS)
+		throw nbad_index(Label(), i);	//nbad_index类不能直接访问LabeledSales类的私有成员，需使用成员方法
+	return Sales::operator[](i);	//和上面同理
+}
+
+double& LabeledSales::operator[](int i)
+{
+	if (i < 0 || i > MONTHS)
+		throw nbad_index(Label(), i);
+	return Sales::operator[](i);
+}
+
+LabeledSales::nbad_index::nbad_index(const string& lb, int ix, const string& s) : Sales::bad_index(ix, s)
+{
+	lbl = lb;
+}
